3d0f1a854fb6c0aacd88db7cd7b3527f78175194
Samantha Fenton <samantha.fenton@live.co.uk>
2023-06-08 14:33:27 +0100

    added app . JS

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+let myName = "Samantha";
+console.log(myName);
+
+let userName = prompt("Hi user :) please tell me your name!");
+console.log(userName);
+
+
+const welcomMsg = alert("Welcome to my page " + userName);
+
+var pageName = "Story of Boo";
+console.log;
+
+
+let myNumber = "12";
+console.log(myNumber);
+
+
+if (myNumber = 12) {
+    console.log ("bonanza");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
